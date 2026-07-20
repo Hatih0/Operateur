5f44991ea156d47c9d0aba8cfee95226f8783d78
Herman <diam.san.rav@gmail.com>
2026-07-20 09:26:27 +0300

    Base Config

--- base.sql
+++ base.sql
@@ -1,19 +1,19 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE client (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     nom TEXT NOT NULL,
     code TEXT NOT NULL,
     numero TEXT NOT NULL
 );
 
 CREATE TABLE type_operation (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     libelle TEXT NOT NULL
 );
 
 CREATE TABLE configuration (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     min REAL NOT NULL,
     max REAL NOT NULL,
     id_type_operation INTEGER NOT NULL,
@@ -21,13 +21,13 @@ CREATE TABLE configuration (
 );
 
 CREATE TABLE operateur (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     nom TEXT NOT NULL,
     mdp TEXT NOT NULL
 );
 
 CREATE TABLE historique (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     id_client INTEGER NOT NULL,
     id_type_operation INTEGER NOT NULL,
     montant REAL NOT NULL,
@@ -35,4 +35,9 @@ CREATE TABLE historique (
     frais REAL NOT NULL,
     FOREIGN KEY (id_client) REFERENCES client(id),
     FOREIGN KEY (id_type_operation) REFERENCES type_operation(id)
+);
+
+CREATE Table prefixe (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    code TEXT
 );
